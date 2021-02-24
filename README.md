42531852c9fd1617463f0a8f30ad2bfa5d7020aa
sphen <sphen@users.noreply.github.com>
2021-02-24 10:58:27 -0700

    update
    updated js (bye bye jquery)
    updated styles
    cleaned up files

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # fry-quotes-frontend
+
 The front-end counterpart to the fryQuotes api with simple styles and transitions.
 
 ## Planned Updates
-- [ ] Add color transitions.
-- [ ] Update to [material design](https://material.google.com/).
+
+- [*] Add color transitions.
+- [*] Update javascript.
--- js/index.js
+++ js/index.js
@@ -1,42 +1,124 @@
-var colors = ["#34495E","#4A89DC", "#967ADC", '#6A50A7', "#9b59b6", "#BF263C", "#DA4453","#D35400", "#E9573F", "#F6BB42", "#F1C40F", "#E0C341", "#8CC152", "#2ABA66", "#37BC9B"];
-var white = "#ECF0F1";
-function getNew() {
+// background colors array
 
-  $.ajax({
-    url: 'https://fry-quotes-api.herokuapp.com/quote/random',
-    success: function(data) {
-      var post = data; // The data is an array of posts. Grab the first one.
+const colors = [
+  '#34495E',
+  '#4A89DC',
+  '#967ADC',
+  '#6A50A7',
+  '#9b59b6',
+  '#BF263C',
+  '#DA4453',
+  '#D35400',
+  '#E9573F',
+  '#F6BB42',
+  '#F1C40F',
+  '#E0C341',
+  '#8CC152',
+  '#2ABA66',
+  '#37BC9B',
+];
 
-      $(".quote-text").animate({
-          opacity: 0
-        }, 500,
-        function() {
-          $(this).animate({
-            opacity: 1
-          }, 500);
-          $('#quote').text(post.quote);
-        });
+// selectors
 
-      $("#author").animate({
-          opacity: 0
-        }, 500,
-        function() {
-          $(this).animate({
-            opacity: 1
-          }, 500);
-          $('#author').text(post.author);
-        });
+const newQuote = document.querySelector('#getQuote');
+const tweetQ = document.querySelector('#tweet');
+const quoteWrap = document.querySelector('.page');
+const qText = document.getElementById('quote');
+const qAuthor = document.getElementById('author');
+const qSeason = document.getElementById('season');
+const qEpisode = document.getElementById('episode');
+const page = document.getElementsByTagName('BODY')[0];
+let curQuote;
 
-      var color = Math.floor(Math.random() * colors.length);
-      $("html body").animate({
-        backgroundColor: colors[color]
-      }, 1000);
+// event listeners
 
-    },
-    cache: false
-  });
+newQuote.addEventListener('click', getQuote);
+tweetQ.addEventListener('click', tweeter);
+window.addEventListener('load', getQuote);
+
+// functions
+
+// change background color to random one from 'colors' array
+// id 5 and 24 get specific colors as easter eggs
+// function bgChange(id) {
+//   console.log('testing123');
+//   let bgColor = Math.floor(Math.random() * colors.length);
+//   console.log(id);
+//   if (id == 5) {
+//     page.style.backgroundColor = '#6A50A7';
+//   } else if (id == 24) {
+//     page.style.backgroundColor = '#4A89DC';
+//   } else {
+//     page.style.backgroundColor = colors[bgColor];
+//   }
+// }
+// bg change arrow function
+const bgCh = (id) => {
+  let bgColor = Math.floor(Math.random() * colors.length);
+  id == 5
+    ? (page.style.backgroundColor = '#6A50A7')
+    : id == 24
+    ? (page.style.backgroundColor = '#6A50A7')
+    : (page.style.backgroundColor = colors[bgColor]);
+};
+
+// retrieve a new random quote from the api using fetch
+function fetchQ() {
+  fetch('https://sphen-new-api.herokuapp.com/quotes/random')
+    .then((res) => res.json())
+    .then((data) => {
+      curQuote = data.quote;
+      let id = data.id;
+      bgCh(id);
+      console.log(data);
+      // delay changing html to avoid pop then fade back in
+      setTimeout( () => {
+        qText.innerHTML = data.quote;
+        qAuthor.innerHTML = data.author;
+        qSeason.innerHTML = 's' + data.season;
+        qEpisode.innerHTML = 'e' + data.episode;
+        quoteWrap.classList.toggle('fadeOut');
+        quoteWrap.classList.toggle('fadeIn');
+      }, 500);
+    });
 }
 
-$(document).ready(getNew);
+// setTimeout(function () {
+//   qText.innerHTML = data.quote;
+//   qAuthor.innerHTML = data.author;
+//   qSeason.innerHTML = 's' + data.season;
+//   qEpisode.innerHTML = 'e' + data.episode;
+//   quoteWrap.classList.toggle('fadeOut');
+//   quoteWrap.classList.toggle('fadeIn');
+// }, 500);
 
-$('#getQuote').click(getNew);
+// fade out content and get a quote
+function getQuote() {
+  quoteWrap.classList.toggle('fadeOut');
+  quoteWrap.classList.toggle('fadeIn');
+  fetchQ();
+}
+// get quote arrow function
+// const getQuote = () => {
+//   quoteWrap.classList.toggle('fadeOut');
+//   quoteWrap.classList.toggle('fadeIn');
+//   fetchQ();
+// }
+
+// tweet the current quote
+function tweeter() {
+  console.log('tweettweet');
+  tweetQ.setAttribute(
+    'href',
+    'https://twitter.com/intent/tweet?hashtags=freecodecamp,quotemachine&related=freecodecamp&text=' +
+      encodeURIComponent('"' + curQuote + '"')
+  );
+}
+// tweet arrow function
+// const tweetit = () => {
+//   tweetQ.setAttribute(
+//     'href',
+//     'https://twitter.com/intent/tweet?hashtags=freecodecamp,quotemachine&related=freecodecamp&text=' +
+//       encodeURIComponent('"' + curQuote + '"')
+//   );
+// }
