3d5db6f8c3acbe8db8e1698624b01364fa142297
sphen <sphen@users.noreply.github.com>
2021-04-13 21:53:50 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -40,19 +40,7 @@ window.addEventListener('load', getQuote);
 
 // change background color to random one from 'colors' array
 // id 5 and 24 get specific colors as easter eggs
-// function bgChange(id) {
-//   console.log('testing123');
-//   let bgColor = Math.floor(Math.random() * colors.length);
-//   console.log(id);
-//   if (id == 5) {
-//     page.style.backgroundColor = '#6A50A7';
-//   } else if (id == 24) {
-//     page.style.backgroundColor = '#4A89DC';
-//   } else {
-//     page.style.backgroundColor = colors[bgColor];
-//   }
-// }
-// bg change arrow function
+
 const bgCh = (id) => {
   let bgColor = Math.floor(Math.random() * colors.length);
   id == 5
@@ -72,7 +60,7 @@ function fetchQ() {
       bgCh(id);
       console.log(data);
       // delay changing html to avoid pop then fade back in
-      setTimeout( () => {
+      setTimeout(() => {
         qText.innerHTML = data.quote;
         qAuthor.innerHTML = data.author;
         qSeason.innerHTML = 's' + data.season;
@@ -83,27 +71,12 @@ function fetchQ() {
     });
 }
 
-// setTimeout(function () {
-//   qText.innerHTML = data.quote;
-//   qAuthor.innerHTML = data.author;
-//   qSeason.innerHTML = 's' + data.season;
-//   qEpisode.innerHTML = 'e' + data.episode;
-//   quoteWrap.classList.toggle('fadeOut');
-//   quoteWrap.classList.toggle('fadeIn');
-// }, 500);
-
 // fade out content and get a quote
 function getQuote() {
   quoteWrap.classList.toggle('fadeOut');
   quoteWrap.classList.toggle('fadeIn');
   fetchQ();
 }
-// get quote arrow function
-// const getQuote = () => {
-//   quoteWrap.classList.toggle('fadeOut');
-//   quoteWrap.classList.toggle('fadeIn');
-//   fetchQ();
-// }
 
 // tweet the current quote
 function tweeter() {
@@ -114,11 +87,3 @@ function tweeter() {
       encodeURIComponent('"' + curQuote + '"')
   );
 }
-// tweet arrow function
-// const tweetit = () => {
-//   tweetQ.setAttribute(
-//     'href',
-//     'https://twitter.com/intent/tweet?hashtags=freecodecamp,quotemachine&related=freecodecamp&text=' +
-//       encodeURIComponent('"' + curQuote + '"')
-//   );
-// }
